f62f1a7dc08a77838c88ca719edc676fa9199fa5
DanielBreaz <danielbreaz@gmail.com>
2022-06-07 11:27:39 +0300

    Tema lectia 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,49 @@
+import static java.lang.System.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        out.println("Hello world!");
+        out.println("Adunarea a doua nr.");
+        int x = 5;
+        int y = 6;
+        int z = x + y;
+        out.println(z);
+        out.println("Impartirea a doua nr.");
+        int a = 10;
+        double b = 5;
+        double c = a / b;
+        out.println(c);
+        out.println(" Printati rezultatul urmatoarelor operatiuni.");
+        int d = -5;
+        double e = 8;
+        double f = 6;
+        double g = d + e * f;
+        out.println(g);
+
+        int h = 55;
+        double i = 9;
+        double j = (h +i) / i;
+        out.println(j);
+
+        int k = 20;
+        double l = 3;
+        double m = k + -l*b / e;
+        out.println(m);
+
+        int n = 15;
+        int o = 2;
+        double p = b + n/l * o - e / l;
+        out.println(p);
+
 
     }
 }
+
+
+
+
+
+
+
+
